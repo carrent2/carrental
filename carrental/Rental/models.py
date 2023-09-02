dbea6d7f24bc1c230823cc8a4b57493c435c9ac2
iraik <karolchrapczynski@wp.pl>
2023-09-02 14:24:37 +0200

    Dodany model Car i UserProfile

--- carrental/Rental/models.py
+++ carrental/Rental/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
+from PIL import Image
+
+
 
 User = settings.AUTH_USER_MODEL
-
 
 class BaseModel(models.Model):
     """Base model."""
@@ -25,8 +27,17 @@ class Car(BaseModel):
     year = models.PositiveIntegerField(validators=[MinValueValidator(2010), MaxValueValidator(2023)], null=False)
     deposit = models.DecimalField(max_digits=6, decimal_places=2, null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    image = models.ImageField(upload_to="images/%Y/%m/%d",blank=True)
 
-    def __str__(self):
+    def save(self, args, **kwargs):
+        super().save(args, **kwargs)
+        if self.image:
+            img = Image.open(self.image.path)
+            max_size = (400, 400)  # Maksymalny rozmiar do którego chcesz skalować obrazek
+            img.thumbnail(max_size)
+            img.save(self.image.path)
+
+    def str(self):
         """Return name of car"""
         return f'{self.brand} {self.model} {self.year}'
 
@@ -35,30 +46,8 @@ class UserProfile(BaseModel):
     """UserProfile model"""
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone = models.CharField(max_length=64, null=False)
-    address = models.CharField(max_length=64, null=False)
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d',blank=True)
 
-    def __str__(self):
-        """Return name of UserProfile"""
-        return f'{self.user} {self.address} {self.phone}'
-
-
-class Rental(BaseModel):
-    """Rental model"""
-
-    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='rentals', null=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rentals', null=False)
-    start_date = models.DateField(null=False)
-    end_date = models.DateField(null=False)
-    price = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
-
-    def __str__(self):
-        """Return name of rental"""
-        return f"{self.car} {self.user} {self.start_date} {self.end_date}"
-
-    def save(self, *args, **kwargs):
-        if not self.pk:
-            self.price = self.car.price * (self.end_date - self.start_date).days
-
-        super(Rental, self).save(*args, **kwargs)
-
+    def str(self):
+        return {self.user.username}
